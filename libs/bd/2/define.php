ee08d8b81f3b8be29d01999badc2090fff687bb0
Fernando Sousa <96151093+feropiso@users.noreply.github.com>
2022-08-15 16:45:28 -0300

    Add files via upload

---
+++ libs/bd/2/define.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BD_PATH_ABS_ANOTACOES', $_SERVER['DOCUMENT_ROOT']."/tarefa_facebook_php/libs/anotacoes/1/"); /*versao 1*/
+define('BD_PATH_ABS_BIB', 	$_SERVER['DOCUMENT_ROOT']."/tarefa_facebook_php/libs/comuns/2/");
+
+define('MYSQL_BD_LOCAL', 'tarefa_php');
+
+define('MYSQL_DNS_LOCAL', 'mysql:host=localhost;dbname='.MYSQL_BD_LOCAL.';');
+define('MYSQL_USER_LOCAL', 'root');
+define('MYSQL_PASSWORD_LOCAL', '');
+
+?>
